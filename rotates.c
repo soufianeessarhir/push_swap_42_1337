1a39efc168e91b1150ce7388822285614b200c57
Soufiane Essarhir <soufianeessarhir@gmail.com>
2024-02-19 21:07:44 +0100

    edit

--- rotates.c
+++ rotates.c
@@ -6,7 +6,7 @@
 /*   By: sessarhi <sessarhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/11 05:41:30 by sessarhi          #+#    #+#             */
-/*   Updated: 2024/02/17 17:28:07 by sessarhi         ###   ########.fr       */
+/*   Updated: 2024/02/19 21:06:35 by sessarhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,9 @@ void ra(t_list **stack_a)
 {
 	int tmp;
 	int tmp1;
-	void *head;
 	
 	tmp = (*stack_a)->content;
 	tmp1 = (*stack_a)->index;
-	head = stack_a;
 	(*stack_a) = (*stack_a)->next;
 	ft_lstadd_back(stack_a,ft_lstnew(tmp,tmp1));
 	write(1, "ra\n",3);
@@ -29,11 +27,9 @@ void rb(t_list **stack_b)
 {
 	int tmp;
 	int tmp1;
-	void *head;
 	
 	tmp = (*stack_b)->content;
 	tmp1 = (*stack_b)->index;
-	head = stack_b;
 	(*stack_b) = (*stack_b)->next;
 	ft_lstadd_back(stack_b,ft_lstnew(tmp,tmp1));
 	write(1, "rb\n",3);
